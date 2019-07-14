07d0580c980ec69d8afa63a45f3cbf673e362e65
ImUrX <urielfontan2002@gmail.com>
2019-07-14 17:27:01 -0300

    i dont understand error

--- compress_builds.sh
+++ compress_builds.sh
@@ -2,5 +2,5 @@
 cd builds
 for dir in ./*
 do
-    cd $dir && tar -caf "../$dir.tar.gz" * && cd - && rm -rf "$dir" && echo "compressed $dir"
+    cd "$dir" && tar -czf "../$dir.tar.gz" * && cd - && rm -rf "$dir" && echo "compressed $dir"
 done
