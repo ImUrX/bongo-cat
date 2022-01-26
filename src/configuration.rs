70916c9783915d89ebf2297a4376eff153644c09
ImUrX <urielfontan2002@gmail.com>
2022-01-26 03:23:42 -0300

    mixed yaml with toml

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,7 @@
 use serde::{Serialize, Deserialize};
-use std::{fs, path};
+use std::{fs, io::{Write, Read}, path};
 
-const CONFIG_FOLDER: &str = "bongocat/";
+const CONFIG_FOLDER: &str = "bongocat";
 const CONFIG_FILENAME: &str = "config.toml";
 const NEUTRAL_DEFAULT: &str = "neutral.png";
 const LEFT_DEFAULT: &str = "left.png";
@@ -14,7 +14,6 @@ lazy_static! {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
-    #[serde(default = "get_default_image_config")]
     images: ImageConfig
 }
 
@@ -27,21 +26,33 @@ pub struct ImageConfig {
 }
 
 impl Config {
+    pub fn create_config() {
+        fs::create_dir_all(CONFIG_PATH.clone()).unwrap();
+        let mut file = fs::File::create(CONFIG_PATH.join(CONFIG_FILENAME)).expect("Couldn't create config file");
+        let vec = toml::to_vec(&Config::default()).unwrap();
+        file.write_all(&vec).unwrap();
+    }
+
     pub fn get_config() -> Self {
         if !CONFIG_PATH.exists() {
-            fs::create_dir_all(CONFIG_PATH.clone()).unwrap();
-            panic!("You need to create the config to make this work!");
+            Self::create_config();
         }
-        let file = fs::File::open(CONFIG_PATH.join(CONFIG_FILENAME)).expect("Config file couldn't be opened");
-        serde_yaml::from_reader(file).unwrap()
+        let mut file = fs::File::open(CONFIG_PATH.join(CONFIG_FILENAME)).expect("Config file couldn't be opened");
+        let mut vec = Vec::new();
+        file.read_to_end(&mut vec).unwrap();
+        toml::from_slice(&vec).unwrap()
     }
 }
 
-fn get_default_image_config() -> ImageConfig {
-    ImageConfig {
-        neutral: NEUTRAL_DEFAULT.into(),
-        left: LEFT_DEFAULT.into(),
-        right: RIGHT_DEFAULT.into(),
-        both: BOTH_DEFAULT.into()
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            images: ImageConfig {
+                neutral: NEUTRAL_DEFAULT.to_string(),
+                left: LEFT_DEFAULT.to_string(),
+                right: RIGHT_DEFAULT.to_string(),
+                both: BOTH_DEFAULT.to_string()
+            }
+        }
     }
 }
