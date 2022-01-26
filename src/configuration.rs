5977abc0d12cab524a5d8cb0c5ad44f1438e5011
ImUrX <urielfontan2002@gmail.com>
2022-01-26 07:18:56 -0300

    cant make it work

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,7 @@
+use crate::keycode_serde::{keycode_vec_deser, keycode_vec_ser};
 use serde::{Serialize, Deserialize};
 use std::{fs, io::{Write, Read}, path};
+use device_query::Keycode;
 
 const CONFIG_FOLDER: &str = "bongocat";
 const CONFIG_FILENAME: &str = "config.toml";
@@ -12,9 +14,27 @@ lazy_static! {
     static ref CONFIG_PATH: path::PathBuf = dirs::config_dir().expect("Couldn't find default config path").join(CONFIG_FOLDER);
 }
 
+pub enum Side {
+    Left,
+    Right,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
-    pub images: ImageConfig
+    pub images: ImageConfig,
+    pub input: InputConfig
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct InputConfig {
+    #[serde(serialize_with = "keycode_vec_ser")]
+    #[serde(deserialize_with = "keycode_vec_deser")]
+    pub keyboard_left: Vec<Keycode>,
+    #[serde(serialize_with = "keycode_vec_ser")]
+    #[serde(deserialize_with = "keycode_vec_deser")]
+    pub keyboard_right: Vec<Keycode>,
+    pub mouse_left: Vec<usize>,
+    pub mouse_right: Vec<usize>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -29,8 +49,8 @@ pub struct ImageConfig {
 impl Config {
     pub fn create_config() {
         fs::create_dir_all(CONFIG_PATH.clone()).unwrap();
-        let mut file = fs::File::create(CONFIG_PATH.join(CONFIG_FILENAME)).expect("Couldn't create config file");
         let vec = toml::to_vec(&Config::default()).unwrap();
+        let mut file = fs::File::create(CONFIG_PATH.join(CONFIG_FILENAME)).expect("Couldn't create config file");
         file.write_all(&vec).unwrap();
     }
 
@@ -43,6 +63,22 @@ impl Config {
         file.read_to_end(&mut vec).unwrap();
         toml::from_slice(&vec).unwrap()
     }
+
+    pub fn get_path(file: &String) -> path::PathBuf {
+        CONFIG_PATH.join(file)
+    }
+
+    pub fn side_of_key(&self, key: &Keycode) -> Option<Side> {
+        if self.input.keyboard_left.contains(key) { return Some(Side::Left); }
+        if self.input.keyboard_right.contains(key) { return Some(Side::Right); }
+        return None;
+    }
+
+    pub fn side_of_button(&self, button: &usize) -> Option<Side> {
+        if self.input.mouse_left.contains(button) { return Some(Side::Left); }
+        if self.input.mouse_right.contains(button) { return Some(Side::Right); }
+        return None;
+    }
 }
 
 impl Default for Config {
@@ -54,6 +90,12 @@ impl Default for Config {
                 left: LEFT_DEFAULT.to_string(),
                 right: RIGHT_DEFAULT.to_string(),
                 both: BOTH_DEFAULT.to_string()
+            },
+            input: InputConfig {
+                keyboard_left: vec!(Keycode::Z.into(), Keycode::A.into()),
+                keyboard_right: vec!(Keycode::C.into(), Keycode::D.into()),
+                mouse_right: vec!(2),
+                mouse_left: vec!(1)
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,99 @@
 #[macro_use]
 extern crate lazy_static;
 
+mod keycode_serde;
 mod configuration;
-use configuration::{Config};
-use fltk::{prelude::*, *};
+use configuration::{Config, Side};
+use std::{path, sync::Arc};
+use fltk::{prelude::*, enums::Color, app, window::Window, frame::Frame, image::SharedImage};
+use device_query::{DeviceEvents, DeviceState};
+
+enum State {
+    Down(Side),
+    Up(Side),
+}
+
+struct BongoCat {
+    app: app::App,
+    config: Config,
+    win: Window,
+    frames: Frames,
+    device_state: DeviceState,
+    receiver: app::Receiver<State>,
+    sender: Arc<app::Sender<State>>,
+}
+
+struct Frames {
+    neutral: Frame,
+    left: Frame,
+    right: Frame,
+    both: Frame,
+}
+
+impl BongoCat {
+    pub fn new() -> Self {
+        let config = Config::get_config();
+        let device_state = DeviceState::new();
+        let (sender, receiver) = app::channel();
+        let app = app::App::default().with_scheme(app::Scheme::Gleam);
+        let mut win = Window::new(100, 100, 600, 600, "Bongo Cat");
+    
+        win.set_color(Color::from_rgba_tuple(config.images.background_color));
+        let neutral = create_frame(&Config::get_path(&config.images.neutral));
+        let mut left = create_frame(&Config::get_path(&config.images.left));
+        left.hide();
+        let mut right = create_frame(&Config::get_path(&config.images.right));
+        right.hide();
+        let mut both = create_frame(&Config::get_path(&config.images.both));
+        both.hide();
+
+        win.make_resizable(true);
+        win.end();
+        win.show();
+        Self {
+            app, win, device_state, config, receiver,
+            sender: Arc::new(sender),
+            frames: Frames {
+                neutral, left, right, both
+            }
+        }
+    }
+
+    pub fn run(mut self) {
+        let _guard = self.device_state.on_key_down(|key| {
+            if let Some(side) = self.config.side_of_key(key) {
+                self.sender.send(State::Down(side));
+            }
+        });
+        let _guard = self.device_state.on_key_up(|key| {
+            if let Some(side) = self.config.side_of_key(key) {
+                self.sender.send(State::Up(side));
+            }
+        });
+        let _guard = self.device_state.on_mouse_down(|button| {
+            if let Some(side) = self.config.side_of_button(button) {
+                self.sender.send(State::Down(side));
+            }
+        });
+        let _guard = self.device_state.on_mouse_up(|button| {
+            if let Some(side) = self.config.side_of_button(button) {
+                self.sender.send(State::Up(side));
+            }
+        });
+        while self.app.wait() {
+
+        }
+    }
+}
+
+fn create_frame(file: &path::Path) -> Frame {
+    let mut frame = Frame::default().size_of_parent().center_of_parent();
+    let image = SharedImage::load(file).unwrap();
+    frame.set_image(Some(image));
+    frame
+}
 
 fn main() {
-    let config = Config::get_config();
-    let app = app::App::default().with_scheme(app::Scheme::Gtk);
-    let mut window = window::Window::new(100, 100, 800, 800, "Bongo Cat");
-    window.set_color(enums::Color::from_rgba_tuple(config.images.background_color))
+    let app = BongoCat::new();
+    app.run();
 }
